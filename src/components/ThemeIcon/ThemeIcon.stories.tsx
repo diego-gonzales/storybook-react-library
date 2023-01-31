b7d704b194831370d1b3fd66086a7fb9eafc4ffc
Diego Gonzales <diegoegj14@gmail.com>
2023-01-31 15:04:32 -0500

    Agregado archivo docs para ThemeIcon

--- src/components/ThemeIcon/ThemeIcon.stories.tsx
+++ src/components/ThemeIcon/ThemeIcon.stories.tsx
@@ -1,10 +1,16 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import { ThemeIcon } from './ThemeIcon';
+import DocsThemeIcon from './DocsThemeIcon.mdx'
 
 export default {
   title: "Components/ThemeIcon",
-  component: ThemeIcon
+  component: ThemeIcon,
+  parameters: {
+    docs: {
+      page: DocsThemeIcon
+    }
+  }
 } as ComponentMeta<typeof ThemeIcon>;
 
 export const Default = <ThemeIcon />;
