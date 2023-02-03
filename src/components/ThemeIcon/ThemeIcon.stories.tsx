4d34d4582ddd1619599cd7d557594837b275bb5d
Diego Gonzales <diegoegj14@gmail.com>
2023-02-03 11:44:24 -0500

    Usando Decorators para agregar contexto al ThemeIcon component

--- src/components/ThemeIcon/ThemeIcon.stories.tsx
+++ src/components/ThemeIcon/ThemeIcon.stories.tsx
@@ -1,13 +1,13 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
-import { ThemeIcon } from "./ThemeIcon";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ThemeIcon as ThemeIconComponent } from "./ThemeIcon";
 import DocsThemeIcon from "./DocsThemeIcon.mdx";
 import { ThemeProvider } from "../../contexts/ThemeContext";
 // import { fireEvent, userEvent, within} from '@storybook/testing-library';
 
 export default {
   title: "Components/ThemeIcon",
-  component: ThemeIcon,
+  component: ThemeIconComponent,
   parameters: {
     docs: {
       page: DocsThemeIcon
@@ -15,18 +15,31 @@ export default {
     actions: {
       handles: ["click"]
     }
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    )
+  ]
+} as ComponentMeta<typeof ThemeIconComponent>;
+
+
+const Template: ComponentStory<typeof ThemeIconComponent> = () => <ThemeIconComponent />;
+
+export const ThemeIcon = Template.bind({});
+ThemeIcon.parameters = {
+  docs: {
+    source: {
+      code: '<ThemeIcon />',
+      language: "js",
+      type: "auto"
+    }
   }
-} as ComponentMeta<typeof ThemeIcon>;
+};
 
-const Template: ComponentStory<typeof ThemeIcon> = () => (
-  <ThemeProvider>
-    <ThemeIcon />
-  </ThemeProvider>
-);
-
-export const Default = Template.bind({});
-
-// Esto me sirve como test, esta historia lo que hara es hacer click automáticamente en el botón una vex se renderiza ña historia
+// Esto me sirve como test, esta historia lo que hara es hacer click automáticamente en el botón una vez se renderiza ña historia
 // export const ClickExample = Template.bind({});
 // ClickExample.play = async ({ canvasElement }) => {
 //   const canvas = within(canvasElement);
